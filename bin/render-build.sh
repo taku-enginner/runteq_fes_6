35bf59b13a6aeef5705a7d4f6d130b5614ce4798
taka4655 <taku.enginner@gmail.com>
2025-03-26 19:50:56 +0900

    fix:render-build.shの修正でdbにseedを流す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,9 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+# bundle exec rake db:migrate　　       # このコードをコメントアウトする
+
+# 以下２行を追記する
+
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:seed
