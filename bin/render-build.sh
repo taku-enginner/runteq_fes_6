71dcab1c85720553ac2e069e15aff8860d6b059f
taka4655 <taku.enginner@gmail.com>
2025-03-26 20:55:07 +0900

    fix:render-bulid.shの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,9 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rake db:migrate　　       # このコードをコメントアウトする
+bundle exec rake db:migrate　　       # このコードをコメントアウトする
 
-# 以下２行を追記する
+# # 以下２行を追記する
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-bundle exec rake db:seed
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+# bundle exec rake db:seed
