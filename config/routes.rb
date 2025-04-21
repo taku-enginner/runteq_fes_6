0d1804d856767746274e9315dcc3bd3dcdfccad8
taka4655 <taku.enginner@gmail.com>
2025-04-21 19:40:45 +0900

    fix/ルート表示の編集

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "top#index"
+  root "applyapp#index"
 end
