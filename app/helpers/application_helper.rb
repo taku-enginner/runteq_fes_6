1e5acb6cecb01bcd663b3a04539ee15cc913cd09
taka_RUNTEQ60a <taku.enginner@gmail.com>
2025-05-11 18:12:03 +0900

    fix:lintエラー解消

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
     [ "52期 とぴ", "50期 MaTTa", "53期 saya", "53期 すみ", "53期 ゆーか" ]
   end
   def neo_metro_labs
-    [ "mogu", "potatoman", "matsushita"]
+    [ "mogu", "potatoman", "matsushita" ]
   end
 end
